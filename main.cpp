6ede5e4ae814b6b42941c5a5cfd370deabe54dc9
WindyAstuti307 <114469509+WindyAstuti307@users.noreply.github.com>
2022-09-28 01:23:12 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <windows.h>
+#ifdef _APPLE_
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <stdlib.h>
+#include <windows.h>
+void objek(void) {
+    glPointSize(4.0);
+    glBegin(GL_POINTS);
+        glColor3f(0.6,1.0,1.0);
+        glVertex3f(0.7, -0.3, 0.0);
+
+        glColor3f(0.8,0.6,0.9);
+        glVertex3f(0.2, 0.6, 0.9);
+
+        glColor3f(0.0,0.3,0.0);
+        glVertex3f(0.4, -0.3, 0.0);
+
+        glColor3f(1.0,0.0,0.5);
+        glVertex3f(0.2, -0.5, 0.0);
+
+        glColor3f(0.5,0.5,1.0);
+        glVertex3f(0.6, -0.7, 0.0);
+
+        glColor3f(0.0,0.0,0.8);
+        glVertex3f(-0.6, 0.3, 0.0);
+    glEnd();
+    glFlush();
+
+}
+void display(void){
+    glClear (GL_COLOR_BUFFER_BIT);
+    glPointSize(2.0);
+    glutSwapBuffers();
+
+}
+
+int main(int argc, char **argv)
+{
+glutInit(&argc, argv);
+glutCreateWindow("E1E121089_gbrTitik");
+glutDisplayFunc(objek);
+glutMainLoop();
+return 0;
+}
+
